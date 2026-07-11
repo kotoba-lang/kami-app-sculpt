82fc53f441ec42fd480883339e35d464f28255e2
Jun Kawasaki <jun784@gmail.com>
2026-07-11 15:14:32 +0900

    build: publish optimized release bundle

--- public/js/cljs-runtime/kotoba.expr.js
+++
@@ -1,5 +0,0 @@
-goog.provide('kotoba.expr');
-kotoba.expr.binops = expr.core.binops;
-kotoba.expr.compile = expr.core.compile;
-
-//# sourceMappingURL=kotoba.expr.js.map
--- public/js/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,2 +0,0 @@
-
-try { kami.sculpt.app.init_BANG_(); } catch (e) { console.error("An error occurred when calling (kami.sculpt.app/init!)"); console.error(e); }
